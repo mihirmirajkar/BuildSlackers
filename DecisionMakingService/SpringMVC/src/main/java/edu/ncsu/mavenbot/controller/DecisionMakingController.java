6a35d649e00ebc168ec1110cbc495504833618af
dwrice <dwrice@ncsu.edu>
2016-10-18 19:55:27 -0400

    Committing integration of pinakin's work with all of the rest of us

--- DecisionMakingService/SpringMVC/src/main/java/edu/ncsu/mavenbot/controller/DecisionMakingController.java
+++ DecisionMakingService/SpringMVC/src/main/java/edu/ncsu/mavenbot/controller/DecisionMakingController.java
@@ -1,7 +1,7 @@
 package edu.ncsu.mavenbot.controller;
 
-import java.util.Random;
 
+import java.util.Random;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,21 +11,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import edu.ncsu.mavenbot.model.GitAdaptor;
+import edu.ncsu.mavenbot.model.*;
 
 @Controller
 @RequestMapping("/decisioncontroller")
 public class DecisionMakingController {
 	
-	@RequestMapping(value="/showcommand/{command}", method = RequestMethod.GET)
+	MavenOverseer mvn = new MavenOverseer();
+	
+	@ResponseBody
+	@RequestMapping(value="/executecommand/{command}", method = RequestMethod.GET)
 	public String getMovie(@PathVariable String command, ModelMap model) {
-
-		model.addAttribute("command", command);
-		return "commandDetails";
-
+		if (command.equals("ls")) {
+			
+			GitAdapter adapter = new GitAdapter();
+			return "{\"responseMessage\":" + adapter.listRepo() + "}";
+		}
+		
+		if (command.contains("cp_")) {
+			//get the project name
+			String projectName = command.split("_")[1];
+			
+			GitAdapter adapter = new GitAdapter();
+			return "{\"responseMessage\":\"" + adapter.changeRepo(projectName) + "\"}";
+		}
+		
+		if (command.equals("ld")) {
+			return "{\"responseMessage\":" + mvn.ListUpdateableDependencies() + "}";
+		}
+		
+		if (command.equals("up")) {
+			return "{\"responseMessage\":" + mvn.FindLatestGoodVersions() + "}";
+		}
+		if (command.contains("up_")) {
+			String after = command.split("_")[1];
+			return "{\"responseMessage\":"+"\"" + 
+					mvn.UpdateDependencyToLatestGoodVersion(Integer.parseInt(after)) + "\"}";
+		}
+		
+		return "";
 	}
 	
-	@RequestMapping(value="getjsonresponse", method = RequestMethod.GET)
+	
+	@RequestMapping(value="/getjsonresponse", method = RequestMethod.GET)
 	public @ResponseBody GitAdaptor getJSONResponse() {
 
 		GitAdaptor gitAdaptor = new GitAdaptor();
